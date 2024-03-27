bd764af22c19fc4dac77bf6e6e242eff882df639
cynthia <cyeeyearn@gmail.com>
2024-03-27 21:03:11 +0300

    Arithmatic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+    let num1=2;
+    let num2=31;
+    let multiply=num1*num2;
+    // Create random number
+    let random = Math.floor(Math.random() *10)+1;
+    console.log(random);
+    //create a mod
+    const mod = 16 % 6;
+    let num3=16;
+    let num4=6
+    //create a max value
+    let max = Math.max(19,20);
+    console.log(max);
